6027bf9f3eb8a0710420362db22460484e153b00
Yohei Shibuya <yoshibu.develop@gmail.com>
2016-11-01 17:57:15 +0900

    update for primitate v4

--- spec/ReactPrimitate.spec.js
+++ spec/ReactPrimitate.spec.js
@@ -1,74 +1,43 @@
 /// <reference path="../node_modules/@types/jasmine/index.d.ts" />
 
 const { jsdom } = require("jsdom");
-const { createElement } = require("react");
-const { renderIntoDocument, Simulate } = require("react-addons-test-utils");
-const { findDOMNode } = require("react-dom");
-const startPrimitate = require("primitate").default
-const initConnector = require("../lib/ReactPrimitate").default
-
-
 // setup
 global.document = jsdom("<!doctype html><html><body></body></html>");
 global.window = document.defaultView;
+global.navigator = document.defaultView.navigator
 
+const { DOM, createElement } = require("react");
+const { renderIntoDocument, Simulate } = require("react-addons-test-utils");
+const { findDOMNode } = require("react-dom");
+const { Primitate } = require("primitate");
+const { PrimitateElement } = require("../lib/ReactPrimitate");
+
+
+function increment(x) { return x + 1; }
+function identity(x) { return x; }
 
 describe("React Primitate", () => {
 
-  it("React component will render when state changed", () => {
-    const { createAction, applyAddon } = startPrimitate({ counter: { count: 0 } }, true);
-    const increment$ = createAction( state => state.counter )( previousState => ({ count: previousState.count + 1 }));
 
-    const connect = applyAddon(initConnector());
+  it("create element", done => {
+    const Counter = Primitate(0);
+    const increment$ = Counter.createAction(identity)(increment);
 
+    const createPElement = PrimitateElement(Counter);
+    const Counter_Element = createPElement(identity)( count => (
+      DOM.div(null, count)
+    ));
 
-    const Counter = ({ count }) => (
-      createElement("div", null, count)
-    );
-    
-    const ConnectedComp = connect( state => state.counter )( state => ({
-      count: state.counter.count
-    }))(Counter);
-
-    const tree = renderIntoDocument(createElement(ConnectedComp));
-    expect(tree.state.count).toBe(0);
-    increment$();
-    expect(tree.state.count).toBe(1)
-    increment$();
-    expect(tree.state.count).toBe(2);
+    const tree = renderIntoDocument(createElement(Counter_Element));
+    setTimeout( () => {
+      expect(findDOMNode(tree).textContent).toBe('0');
+    }, 20);
+    setTimeout( () => {
+      expect(increment$()).toBe(1);
+    });
+    setTimeout( () => {
+      expect(findDOMNode(tree).textContent).toBe('1');
+      done();
+    }, 30);
   });
-
-
-  it("can pass actions", () => {
-    const { createAction, applyAddon } = startPrimitate({ counter: { count: 0 } }, true);
-
-    const increment$ = createAction( state => state.counter )( counter => ({ count: counter.count + 1 }));
-    const connect = applyAddon(initConnector({ increment$ }));
-
-
-    const Counter = ({ count, increment }) => (
-      createElement("div", null
-      , createElement("button", { onClick: increment })
-      , createElement("p", null, count)
-      )
-    );
-
-    const ConnectedComp = connect( state => state.counter )( (state, actions) => ({
-      count: state.counter.count
-    , increment: actions.increment$
-    }))(Counter);
-
-
-    const tree = renderIntoDocument(createElement(ConnectedComp));
-    const btn = findDOMNode(tree).children[0];
-    const p = findDOMNode(tree).children[1];
-
-    expect(p.textContent).toBe("0");
-    Simulate.click(btn);
-    expect(p.textContent).toBe("1");
-    Simulate.click(btn);
-    expect(p.textContent).toBe("2");
-    
-  });
-  
 });
--- src/ReactPrimitate.ts
+++ src/ReactPrimitate.ts
@@ -1,45 +1,37 @@
-import { createAction, subscribe } from "primitate"
 import * as React from "react"
+import { PrimitateClass } from "primitate"
 
-export declare type connect<ACTION, STATE> = (...pickers: ((state: STATE) => any)[]) => 
-  <P>(getProps: (state: STATE, acctions?: ACTION) => P) => <PROP>(
-    wrappedComponent: React.ComponentClass<P> | React.StatelessComponent<P>
-  ) => React.ComponentClass<PROP> 
+function createPrimitateElement<State>(
+  PrimitateItem: PrimitateClass<State>
+, pickers: ((state: State) => any)[]
+, wrappedElement: (state: State) => React.ReactElement<{}>): React.ComponentClass<{}> {
+  return class extends React.Component<{}, { __PrimitateElm: State }> {
+    private unsubscribe: () => void
+    
+    constructor(props: State) { super(props); }
+    
+    componentWillMount() {
+      const subscriber =
+        <(listener: (state: State) => void) => () => void>PrimitateItem.subscribe.apply(PrimitateItem, pickers);
+      this.unsubscribe = subscriber( state => this.setState({ __PrimitateElm: state}) );
+    }
 
-export default function initConnector<A extends {}>(actions: A = <A>{}) {
-  return <S>(createAction: createAction<S>, subscribe: subscribe<S>) => {
-    return (...pickers: ((state: S) => any)[]) => {
-      return <P>(getProps: (state: S, actions: A) => P) => {
-        return <PROP>(
-          wrappedComponent: React.ComponentClass<P> | React.StatelessComponent<P>
-        ): React.ComponentClass<PROP> => {
+    componentWillUnmount() {
+      this.unsubscribe();
+    }
 
-          return class extends React.Component<PROP, P> {
-            unsubscribe: () => void
-            
-            constructor(props: PROP) { super(props); }
-            
-            componentWillMount() {
-              const subscriber = <(listener: (state: S) => void) => () => void>subscribe.apply(null, pickers);
-              this.unsubscribe = subscriber( state => {
-                const props = getProps(state, actions);
-                this.setState(props);
-              });
-            }
+    render() {
+      if (this.state === null)
+        return React.DOM.span();
+      return wrappedElement(this.state.__PrimitateElm);
+    }
+  }
+}
 
-            componentWillUnmount() {
-              this.unsubscribe();
-            }
-
-          render() {
-              const props: { [key: string]: any} = {};
-              for (let key in this.props) props[key] = (<{ [key: string]: any}>this.props)[key];
-              for (let key in this.state) props[key] = (<{ [key: string]: any}>this.state)[key];
-              return React.createElement(wrappedComponent, <P>props);
-            }
-          }
-        }
-      }
+export function PrimitateElement<State>(PrimitateItem: PrimitateClass<State>) {
+  return (...pickers: ((state: State) => any)[]) => {
+    return <PROPS>(wrappedElement: (state: State) => React.ReactElement<PROPS>): React.ComponentClass<{}> => {
+      return createPrimitateElement(PrimitateItem, pickers, wrappedElement);
     }
   }
 }
