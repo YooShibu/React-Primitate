dacebf3990cf298b11f1969a24f06bad2624334b
Yohei Shibuya <yoshibu.develop@gmail.com>
2016-11-08 23:00:30 +0900

    remove tmp file

--- src/tmp.ts
+++
@@ -1,7 +0,0 @@
-import { Primitate, PrimitateClass } from "primitate"
-import { PrimitateComponent } from "./ReactPrimitate"
-
-const Counter = Primitate({ count: 0 });
-
-const createComp = PrimitateComponent(Counter);
-
